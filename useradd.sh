7fbf18b86700828abcfd221d7479d60ed7c49be9
ArpanaM <muriki.arpana@gmail.com>
2025-02-13 05:59:55 +0530

    My First File

--- useradd.sh
+++ useradd.sh
@@ -3,4 +3,4 @@ username=riya
 useradd ${username}
 echo "Adding user- ${username}"
 echo "Successfully added user--${username}"
-echo password | passwd--stdin ${username}
+echo password | passwd --stdin ${username}
