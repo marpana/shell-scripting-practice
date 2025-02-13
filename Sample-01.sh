9585219f7b26a067c83e9e029a36a94e1ae0ebcf
ArpanaM <muriki.arpana@gmail.com>
2025-02-13 05:47:17 +0530

    My First File

--- Sample-01.sh
+++ Sample-01.sh
@@ -3,3 +3,8 @@ echo Hello World
 echo colors
 
 echo -e "\e[31mHello in Red color\e[0m"
+echo -e "\e[32mHello in Green color\e[0m"
+echo -e "\e[33mHello in Yellow color\e[0m"
+echo -e "\e[34mHello in Blue color\e[0m"
+echo -e "\e[35mHello in Magenta color\e[0m"
+echo -e "\e[36mHello in Cyan color\e[0m"
