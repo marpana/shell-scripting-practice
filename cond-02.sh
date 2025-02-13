30f54efb84f364add56c31c5b35e7406aa48e5b3
ArpanaM <muriki.arpana@gmail.com>
2025-02-13 06:18:32 +0530

    My First File

--- cond-02.sh
+++ cond-02.sh
@@ -1,6 +1,8 @@
 read -p "Enter your age--" age
 if [ "${age}" -lt 18 ]; then
   echo You are minor
+  elif [ "${age}" -lt 60 ]; then
+    echo you are senior citizen
   else
     echo You are major
 fi
