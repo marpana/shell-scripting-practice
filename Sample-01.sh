cd40649337a71bad527ec60f32003db2fa739613
ArpanaM <muriki.arpana@gmail.com>
2025-02-13 05:49:58 +0530

    My First File

--- Sample-01.sh
+++ Sample-01.sh
@@ -8,3 +8,11 @@ echo -e "\e[33mHello in Yellow color\e[0m"
 echo -e "\e[34mHello in Blue color\e[0m"
 echo -e "\e[35mHello in Magenta color\e[0m"
 echo -e "\e[36mHello in Cyan color\e[0m"
+
+#Variables
+a=100
+b=Apple
+c=True
+
+echo a=$a
+b=${b}
