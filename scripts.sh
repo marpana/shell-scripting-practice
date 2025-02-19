9712bfcfb22e729c9df995d8d4774b36930b5ce1
ArpanaM <muriki.arpana@gmail.com>
2025-02-19 05:50:11 +0530

    open ports

--- scripts.sh
+++ scripts.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-echo "IPV4 address"
-ip -4 addr show | grep inet
-echo "************"
-ip -6 addr show | grep inet6
+#echo "IPV4 address"
+#ip -4 addr show | grep inet
+#echo "************"
+#ip -6 addr show | grep inet6
 
 
-#!/bin/bash
-read -p "Enter url---" url
-echo $url
-ping -c 3 $url > /dev/null
 
-if [ $? -eq 0 ]; then
-  echo "Internet is reachable"
-  else
-    echo "Internet is not reachable"
-    fi
+#read -p "Enter url---" url
+#echo $url
+#ping -c 3 $url > /dev/null
+#
+#if [ $? -eq 0 ]; then
+#  echo "Internet is reachable"
+#  else
+#    echo "Internet is not reachable"
+#    fi
+
+
+echo "display open ports"
+echo "**************************"
+sudo yum install net-tools
+netstat -tuln
 
